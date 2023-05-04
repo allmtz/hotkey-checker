4fdee9fc1278e9936ec621766958b6494500907b
Allan Martinez <martinezallan123@gmail.com>
2023-05-03 22:51:01 -0700

    refactor to work with Brave and Chrome

--- chromeHotkeys.js
+++ chromeHotkeys.js
@@ -1,27 +1,29 @@
-// TODO
-// format raw HKs here? if so ==> "command" needs to be changed to "Meta" & "option" needs to be changed to "Alt"
-
 import { chromeHk } from "./WIPchormeHotkeys";
+import { braveHK } from "./braveHotkeys";
 
-let chromeHksObj = {};
+function createObject(hkString) {
+  let hkObj = {};
 
-// Create an object out of the shortcuts to quickly determine if the user inputs a valid hotkey
-chromeHk.split("\n").forEach((line) => {
-  const shortcut = line.split(":");
+  // Create an object out of the shortcuts to quickly determine if the user inputs a valid hotkey
+  hkString.split("\n").forEach((line) => {
+    const shortcut = line.split(":");
 
-  const description = shortcut[0].trim();
-  const hotkey = shortcut[1].trim();
+    const description = shortcut[0].trim();
+    const hotkey = shortcut[1].trim();
 
-  if (hotkey.includes("or")) {
-    // Create multiple entries for shortcuts that have multiple hotkeys
-    const hotkeyVariants = hotkey.split("or");
+    if (hotkey.includes("or")) {
+      // Create multiple entries for shortcuts that have multiple hotkeys
+      const hotkeyVariants = hotkey.split("or");
 
-    hotkeyVariants.forEach(
-      (variant) => (chromeHksObj[variant.trim()] = description)
-    );
-  } else {
-    chromeHksObj[hotkey] = description;
-  }
-});
+      hotkeyVariants.forEach(
+        (variant) => (hkObj[variant.trim()] = description)
+      );
+    } else {
+      hkObj[hotkey] = description;
+    }
+  });
+  return hkObj;
+}
 
-export default chromeHksObj;
+export const chromeHksObj = createObject(chromeHk);
+export const braveHksObj = createObject(braveHK);
