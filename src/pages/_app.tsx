b367e2ccb9c4fe3431766c573616dcd6546add00
Allan <martinezallan123@gmail.com>
2023-05-17 10:08:45 -0700

    added font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const noto = Plus_Jakarta_Sans({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={noto.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
