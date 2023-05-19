bd3a4223b3b237710f9a0f8444d37b20c4dcc6a6
Allan Martinez <martinezallan123@gmail.com>
2023-05-19 10:15:17 -0700

    added windows hks for firefox

--- hotkeys/firefoxHotkeys.js
+++ hotkeys/firefoxHotkeys.js
@@ -3,7 +3,8 @@ import { standardize } from "./regex";
 // from https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly#firefox:mac:fx114
 // version 114 for Mac
 // updated 5/9/23
-const rawFirefoxHks = `Back : command + ←
+const rawFirefoxHks = `FOR: MAC
+Back : command + ←
 Back : command + [
 Forward : command + →
 Forward : command + ]
@@ -141,6 +142,147 @@ Seek forward 10 % : command + →
 Move the colored border to left, right, first or last tab. : command + arrow keys
 Move the colored border to left, right, first or last tab. : command + home
 Move the colored border to left, right, first or last tab. : command + end
-Select or deselect the tab with the colored border. The other tabs stay selected or deselected. : command + space`;
+Select or deselect the tab with the colored border. The other tabs stay selected or deselected. : command + space
+FOR:WINDOWS
+Back : Alt + ←
+Forward : Alt + →
+Forward : Shift + backspace
+Home : Alt + home
+Open File : Ctrl + o
+Reload : Ctrl + r
+Reload (override cache) : Ctrl + f5
+Reload (override cache) : Ctrl + Shift + r
+Focus Previous Link or Input Field : Shift + tab
+Go Up a Screen : Shift + space bar
+Go to Bottom of Page : Ctrl + ↓
+Go to Top of Page : Ctrl + ↑
+Move to Previous Frame, Pop-Up : Shift + f6
+Print : Ctrl + p
+Save Focused Link : Alt + enter
+Save Page As : Ctrl + s
+Zoom In : Ctrl + +
+Zoom Out : Ctrl + -
+Zoom Reset : Ctrl + 0
+Copy : Ctrl + c
+Cut : Ctrl + x
+Delete Word to the Left : Ctrl + backspace
+Delete Word to the Right : Ctrl + del
+Go one Word to the Left : Ctrl + ←
+Go one Word to the Right : Ctrl + →
+Go to Beginning of Line : Ctrl + ↑
+Go to End of Line : Ctrl + ↓
+Go to Beginning of Text : Ctrl + home
+Go to End of Text : Ctrl + end
+Paste : Ctrl + v
+Paste (as plain text) : Ctrl + Shift + v
+Redo : Ctrl + y
+Redo : Ctrl + Shift + z
+Select All : Ctrl + a
+Undo : Ctrl + z
+Find in This Page : Ctrl + f
+Find Again : Ctrl + g
+Find Previous : Shift + f3
+Find Previous : Ctrl + Shift + g
+Switch Search Engine : Alt + ↓
+Switch Search Engine : Alt + ↑
+Focus Address bar for Web Search with Default Search Engine : Ctrl + k
+Focus Address bar for Web Search with Default Search Engine : Ctrl + e
+Focus Search bar : Ctrl + k
+Focus Search bar : Ctrl + e
+Change the Default Search Engine : Ctrl + ↓
+Change the Default Search Engine : Ctrl + ↑
+View menu to switch, add or manage search engines : Alt + ↓
+View menu to switch, add or manage search engines : Alt + ↑
+Close Tab : Ctrl + w
+Close Tab : Ctrl + f4
+Close Window : Ctrl + Shift + w
+Close Window : Alt + f4
+Cycle through Tabs in Recently Used Order : Ctrl + tab
+Exit : Ctrl + Shift + q
+Go one Tab to the Left : Ctrl + page up
+Go one Tab to the Left : Ctrl + Shift + tab
+Go one Tab to the Right : Ctrl + page down
+Go one Tab to the Right : Ctrl + tab
+Go to Tab 1 to 8 : Ctrl + 1 to 8
+Go to Last Tab : Ctrl + 9
+Move Tab Left : Ctrl + Shift + page up
+Move Tab Right : Ctrl + Shift + page down
+Move Tab to start : Ctrl + Shift + home
+Move Tab to end : Ctrl + Shift + end
+Mute/Unmute Audio : Ctrl + m
+New Tab : Ctrl + t
+New Window : Ctrl + n
+New Private Window : Ctrl + Shift + p
+Open Address or Search in New Background Tab : Alt + Shift + enter
+Open Address or Search in New Foreground Tab : Alt + enter
+Open Address or Search in New Window : Shift + enter
+Open Search in New Background Tab : Ctrl + enter
+Open Search in New Foreground Tab : Ctrl + Shift + enter
+Open Focused Bookmark in New Foreground Tab : Ctrl + enter
+Open Focused Bookmark in New Background Tab : Ctrl + Shift + enter
+Open Focused Link in New Background Tab : Ctrl + enter
+Open Focused Link in New Foreground Tab : Ctrl + Shift + enter
+Open Focused Bookmark or Link in New Window : Shift + enter
+Show All Tabs : Ctrl + Shift + tab
+Undo Close Tab : Ctrl + Shift + t
+Undo Close Window : Ctrl + Shift + n
+Moves the URL left or right (if cursor is in the address bar) : Ctrl + Shift + x
+History sidebar : Ctrl + h
+Library window (History) : Ctrl + Shift + h
+Clear Recent History : Ctrl + Shift + del
+History sidebar : command + shift + h
+Clear Recent History : command + shift + delete
+Bookmark All Tabs : Ctrl + Shift + d
+Bookmark This Page : Ctrl + d
+Bookmarks sidebar : Ctrl + b
+Show/hide the Bookmarks toolbar : Ctrl + Shift + b
+Show All Bookmarks (Library Window) : Ctrl + Shift + o
+Downloads : Ctrl + j
+Add-ons : Ctrl + Shift + a
+Toggle Developer Tools : Ctrl + Shift + i
+Web Console : Ctrl + Shift + k
+Inspector : Ctrl + Shift + c
+Take a screenshot : Ctrl + Shift + s
+Style Editor : Shift + f7
+Profiler : Shift + f5
+Network : Ctrl + Shift + e
+Responsive Design View : Ctrl + Shift + m
+Page Source : Ctrl + u
+Browser Console : Ctrl + Shift + j
+Page Info : Ctrl + i
+Zoom in : Ctrl + +
+Zoom out : Ctrl + -
+Automatic Zoom : Ctrl + 0
+Rotate counterclockwise : Shift + r
+Switch to Presentation Mode : Ctrl + Alt + p
+Focus the Page Number input box : Ctrl + Alt + g
+Complete .com Address : Ctrl + enter
+Delete Selected Autocomplete Entry : Shift + del
+Toggle Menu Bar activation (showing it temporarily when hidden) : Alt or f10
+Focus Address Bar : Alt + d
+Focus Address Bar : Ctrl + l
+Focus Search Field in Library : Ctrl + f
+Close a Menu : Alt
+Toggle Context Menu : Shift + f10
+Complete .com Address : control + return
+Delete Selected Autocomplete Entry : shift + delete
+Complete .com Address : Ctrl + enter
+Complete .net Address : Shift + enter
+Complete .org Address : Ctrl + Shift + enter
+Toggle Menu Bar activation (showing it temporarily when hidden) : Alt or f10alt or f10
+Toggle Reader Mode : Ctrl + Alt + rf9ctrl + Alt + r
+Toggle Context Menu : Shift + f10
+Complete .com Address : command + return
+Complete .net Address : shift + return
+Complete .org Address : command + shift + return
+Delete Selected Autocomplete Entry : shift + delete
+Mute audio : Ctrl + ↓
+Unmute audio : Ctrl + ↑
+Seek back 10 % : Ctrl + ←
+Seek forward 10 % : Ctrl + →
+Move the colored border to left, right, first or last tab. : Ctrl + arrow keys
+Move the colored border to left, right, first or last tab. : Ctrl + home
+Move the colored border to left, right, first or last tab. : Ctrl + end
+Select or deselect the tab with the colored border. The other tabs stay selected or deselected. : Ctrl + space`;
 
 export const firefoxHks = standardize(rawFirefoxHks);
