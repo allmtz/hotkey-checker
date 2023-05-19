af41d631c2e430ffa36d0239737bf5cfc9938b0e
Allan Martinez <martinezallan123@gmail.com>
2023-05-19 10:34:42 -0700

    wording + fixing links

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ export default function About() {
       <div>
         <p className="about-header">What is this?</p>
         <p className="about-text">
-          a simple tool that allows developers to quickly find reserved hotkeys
+          A simple tool that allows developers to quickly find reserved hotkeys
           for various browsers.
         </p>
       </div>
@@ -12,10 +12,10 @@ export default function About() {
       <div>
         <p className="about-header">Why?</p>
         <p className="about-text">
-          Depending on how many browsers you're targeting, finding non-reserved
-          cross-browser compatible hotkeys can involve a tedious amount of
-          searching and comparing for each individual browser. This tool aims to
-          make that process smoother.
+          Finding non-reserved cross-browser compatible hotkeys can be a tedious
+          process, involving a significant amount of searching and comparing for
+          each individual browser you&apos;re targeting. This tool aims to make
+          that process smoother.
         </p>
       </div>
 
@@ -24,13 +24,18 @@ export default function About() {
         <ul>
           <li>
             <p className="about-text">
-              <a href="https://www.toptal.com/developers/keycode/a">Toptal</a> :
-              JavaScript key code checker
+              <a
+                href="https://www.toptal.com/developers/keycode/a"
+                target="_blank"
+              >
+                Toptal
+              </a>{" "}
+              : JavaScript key code checker
             </p>
           </li>
           <li>
             <p className="about-text">
-              <a href="https://www.toptal.com/developers/keycode/a">
+              <a href="https://mycolor.space/" target="_blank">
                 ColorSpace
               </a>{" "}
               : Color palette generator and gradient tool
