2fcebe1acfe3f0a356df8463ebe0e883f83e563e
Allan Martinez <martinezallan123@gmail.com>
2023-05-20 17:31:09 -0700

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Nav } from "@/components/Nav";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const jakarta = Plus_Jakarta_Sans({ weight: "400", subsets: ["latin"] });
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <main className={jakarta.className}>
       <Nav />
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
